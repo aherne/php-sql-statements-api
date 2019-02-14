cc161716a4258c96cdc395cf879039c441078496
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2019-02-14 11:10:09 +0200

    * completed document

--- plugins/MySQL/MySQLSelect.php
+++ plugins/MySQL/MySQLSelect.php
@@ -18,4 +18,45 @@ require_once(dirname(dirname(__DIR__))."/src/Select.php");
  */
 class MySQLSelect extends Select
 {
+    private $calcFoundRows;
+
+    /**
+     * Appends a SQL_CALC_FOUND_ROWS option to SELECT
+     */
+    public function setCalcFoundRows() {
+        $this->calcFoundRows = true;
+    }
+
+    /**
+     * Gets query to retrieve found rows after a SELECT with SQL_CALC_FOUND_ROWS has ran
+     *
+     * @return string
+     */
+    public function getCalcFoundRows() {
+        return "SELECT FOUND_ROWS()";
+    }
+
+    /**
+     * Compiles SQL statement based on data collected in class fields.
+     *
+     * @return string SQL that results from conversion
+     */
+    public function toString() {
+        $strOutput =
+            "SELECT ".($this->isDistinct?" DISTINCT":"").($this->calcFoundRows?" SQL_CALC_FOUND_ROWS":"").
+            "\r\n".($this->columns?$this->columns->toString():"*").
+            "\r\n"."FROM ".$this->table;
+        if(sizeof($this->joins)>0) 	{
+            foreach($this->joins as $join) {
+                $strOutput .= "\r\n".$join->toString();
+            }
+        }
+        $strOutput .=
+            ($this->where && !$this->where->isEmpty()?"\r\nWHERE ".$this->where->toString():"").
+            ($this->groupBy && !$this->groupBy->isEmpty()?"\r\nGROUP BY ".$this->groupBy->toString():"").
+            ($this->having && !$this->having->isEmpty()?"\r\nHAVING ".$this->having->toString():"").
+            ($this->orderBy && !$this->orderBy->isEmpty()?"\r\nORDER BY ".$this->orderBy->toString():"").
+            ($this->limit?"\r\nLIMIT ".$this->limit->toString():"");
+        return $strOutput;
+    }
 }
